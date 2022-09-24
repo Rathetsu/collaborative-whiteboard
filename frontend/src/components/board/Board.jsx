6b344186a31bcfb013f3e3690e256b2ea59b0d5e
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2022-09-24 17:37:52 +0200

    initial implementation of basic components

---
+++ frontend/src/components/board/Board.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Board extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div className="board">
+				<h1> hello </h1>
+			</div>
+		);
+	}
+
+}
+export default Board;
---
+++ frontend/src/components/container/Container.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+import Board from "../board/Board";
+
+class Container extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+
+	render() {
+		return (
+			<div className="container">
+				<div className="tool-bar-container">
+
+				</div>
+
+				<div className="board-container">
+					<Board />
+				</div>
+
+
+			</div>
+		);
+	}
+}
+export default Container;
